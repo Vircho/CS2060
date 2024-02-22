9cd243eff3dc75c059f57fcd29fd601e89db4df5
Vircho <140011936+Vircho@users.noreply.github.com>
2024-02-22 14:45:38 -0700

    Finished Code
    Code now prints final survey results, calculates he average of each category, and prints those averages.

--- CS2060ClassCodeSpring2024-main/examples/homework/GE04RenterSurveyWhite.c
+++ CS2060ClassCodeSpring2024-main/examples/homework/GE04RenterSurveyWhite.c
@@ -22,9 +22,9 @@
 //Function prototypes
 void printCategories(const char* categories[], size_t totalCategories);
 void getRatings(int survey[SURVEY_ROWS][SURVEY_COLUMNS], size_t rows, size_t columns, const char* categories[], int user);
-void printSurveyResults();
-void calculateCategoryAverages();
-void printCategoryData();
+void printSurveyResults(int survey[SURVEY_ROWS][SURVEY_COLUMNS], int amountTakenSurveys);
+void calculateCategoryAverages(double average[], int amountCategories, int surveyTakers, int survey[SURVEY_ROWS][SURVEY_COLUMNS]);
+void printCategoryData(double average[], size_t size);
 
 void clearBuffer();
 
@@ -32,18 +32,31 @@ int main(void) {
 
 	//Create arrays
 	int rideshareSurvey[SURVEY_ROWS][SURVEY_COLUMNS];
-	int categoryAverages[SURVEY_CATEGORIES];
+	double categoryAverages[SURVEY_CATEGORIES];
 	const char* surveyCategories[SURVEY_CATEGORIES] = { "Safety", "Cleanliness", "Comfort" };
 
     //Initialize other variables
-    int surveyTaker = 1;
+    int surveyTaker = 0;
     
-    while (surveyTaker <= SURVEY_ROWS) {
+    while (surveyTaker < SURVEY_ROWS) {
 
         //Get ratings from the user
         getRatings(rideshareSurvey, SURVEY_ROWS, SURVEY_COLUMNS, surveyCategories, surveyTaker);
 
+        surveyTaker++;
     }
+
+    //Once enough surveys have been taken, print out statistics
+    //Print all surveys
+    printSurveyResults(rideshareSurvey, surveyTaker);
+
+    //Calculate the averages of all categories
+    calculateCategoryAverages(categoryAverages, SURVEY_CATEGORIES, surveyTaker, rideshareSurvey);
+
+    //Print the averages of all categories
+    printCategories(surveyCategories, SURVEY_CATEGORIES);
+    printCategoryData(categoryAverages, SURVEY_CATEGORIES);
+
 }
 
 void printCategories(const char* categories[], size_t totalCategories)
@@ -57,6 +70,13 @@ void printCategories(const char* categories[], size_t totalCategories)
     puts(""); // start new line of output
 }
 
+// function getRatings()
+/*
+* -> Runs through the columns(categories) of the survey 2d array and fills one row with valid user-inputted scores between two values\
+* returns: void
+* parameters: (survey: 2d array that contains a row of user-inputted scores in category columns) (rows, amount of rows of survey)
+* (columns: amount of columns of survey) (categories: array that holds the names of the categories in strings) (user: number of the current user)
+*/
 void getRatings(int survey[SURVEY_ROWS][SURVEY_COLUMNS], size_t rows, size_t columns, const char* categories[], int user) {
 
     //Prompts for the user to input a score and lets them know what they can input
@@ -101,20 +121,75 @@ void getRatings(int survey[SURVEY_ROWS][SURVEY_COLUMNS], size_t rows, size_t col
         // Once valid data is obtained, put the data into the 2d array at current collumn in the for-loop and row current user as defined in parameters
         survey[user][place] = userScore;
     }
+} // end getRatings()
+
+// function printSurveyResults()
+/*
+* -> Prints the 2d array survey row by row
+* returns: void
+* parameters: (survey: 2d array being printed) (amountTakenSurvey: the amount of rows to print)
+*/
+void printSurveyResults(int survey[SURVEY_ROWS][SURVEY_COLUMNS], int amountTakenSurveys) {
+
+    int surveyBeingPrinted = 0;
+    while (surveyBeingPrinted < amountTakenSurveys) {
+
+        int thisCategory1 = survey[surveyBeingPrinted][0];
+        int thisCategory2 = survey[surveyBeingPrinted][1];
+        int thisCategory3 = survey[surveyBeingPrinted][2];
+
+        printf("Survey %d:\t%d\t%d\t%d", surveyBeingPrinted + 1, thisCategory1, thisCategory2, thisCategory3);
+        puts("");
+        surveyBeingPrinted++;
+    }
+
 }
 
-void printSurveyResults() {
+// function calculateCategoryAverages()
+/*
+* -> calculates the average of each column in 2d array survey
+* returns: void
+* parameters: (average: array to store the calculated averages) (amountCategories: columns in the 2d array)
+* (surveyTakers: rows to go through in the 2d array) (survey: 2d array used to calculate the average of each column)
+*/
+void calculateCategoryAverages(double average[], int amountCategories, int surveyTakers, int survey[SURVEY_ROWS][SURVEY_COLUMNS]) {
+
+    for (size_t categories = 0; categories < amountCategories; categories++) {
+
+        double categoryAverage = 0;
+        
+        for (size_t score = 0; score < surveyTakers; score ++) {
+
+            categoryAverage += survey[score][categories];
+
+        }
+
+        categoryAverage = (double)(categoryAverage / surveyTakers);
+        average[categories] = categoryAverage;
+
+    }
+}
+
+// function printCategoryData()
+/*
+* -> prints the data in 1d array average
+* returns: void
+* parameters: (average: array to be printed)
+*/
+void printCategoryData(double average[], size_t size) {
+
+    for (size_t place = 0; place < size; place++) {
+        printf("%.1f\t", average[place]);
+    }
 
 }
 
-void calculateCategoryAverages() {
-
-}
-
-void printCategoryData() {
-
-}
-
+// function clearBuffer()
+/*
+* -> clears the buffer when using scanf()
+* returns: void
+* parameters: none
+*/
 void clearBuffer() {
     while ((getchar()) != '\n');
 }
